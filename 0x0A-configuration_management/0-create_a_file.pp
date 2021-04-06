70c45f671c0cfac09c437be7752fd9fe6f737c19
Peter5793 <masaba.peter68@gmail.com>
2021-04-06 21:52:19 +0300

    change mode name

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
---Create file
+#Create a file
 file { '/etc':
 path       => '/tmp/holberton',
-permission => '0744',
+mode       => '0744',
 owner      => 'www-data',
 group      => 'www-data',
 content    => 'I love Puppet'
