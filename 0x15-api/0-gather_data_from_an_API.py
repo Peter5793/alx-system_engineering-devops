f3a9e0a3b3441f2ba512eae39cae1f463f6913bd
Peter5793 <masaba.peter68@gmail.com>
2021-05-03 23:16:36 +0300

    add first api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Python script with REST API, for a given employee ID
+returns Information about his/her TODO list
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    """ get user """
+    for user in users:
+        if user.get('id') == user_id:
+            n_user = user
+
+    """get todos """
+    task_list = []
+    for task in todos:
+        if task.get('userId') == user_id:
+            task_list.append(task)
+
+    """find completed todos """
+    completed_tasks = []
+    for task in task_list:
+        if task.get('completed') is True:
+            completed_tasks.append(task)
+
+    """ print based on format stated"""
+    print("Employee {} is done with tasks({}/{}):".format(n_user.get('name'),
+                                                          len(completed_tasks),
+                                                          len(task_list)))
+
+    for print_task in completed_tasks:
+        print("\t {}".format(print_task.get('title')))
