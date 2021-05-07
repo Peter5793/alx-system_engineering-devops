3a81b7ef1bcc931efe21793eab9c43ac7136d167
Peter5793 <masaba.peter68@gmail.com>
2021-05-07 11:56:02 +0300

    add json and csv exports

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+python script that is using RESTapi for a given
+employee id, returns information about is/her
+TODO list
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    d_users = {}
+    d_todos = {}
+
+    for user in users:
+        u_id = user.get('id')
+        d_user[u_id] =[]
+        d_username[u_id] = user.get('username')
+
+    for task in todos:
+        task_dict ={}
+        u_id = task.get('userId')
+        task_dict['task'] =task.get('title')
+        task_dict['completed'] = task.get('completed')
+        task_dict['username'] = d_username.get(u_id)
+        d_suer[u_id].append(task_dict)
+
+    with open("todo_all_employees.json", 'w') as file:
+        json.dup(d_users, file)
