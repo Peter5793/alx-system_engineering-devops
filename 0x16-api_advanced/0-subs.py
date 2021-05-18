5bab99d9109d479df35142d92ec5f4882f35c741
Peter5793 <masaba.peter68@gmail.com>
2021-05-18 21:43:47 +0300

    add files

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns the number of subscribers
+"""
+import json
+import requests
+
+def number_of_subscribers(subreddit):
+    """get request to obtain subscribers"""
+    headers = {'User-Agent': 'linux:com.myapp:v.1'}
+    response = requests.get('https://www.reddit.com/r/{}/about'.
+                           format(subreddit), headers=headers)
+
+    if response.status_code == 404:
+                return 0
+
+    n_response = requests.get('https://www.reddit.com/r/{}/about.json'.
+                              format(subreddit), headers=headers).json()
+
+    return n_response.get('data').get('subscribers')
